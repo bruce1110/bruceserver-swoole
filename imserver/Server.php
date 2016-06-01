98c74ee4ded7ae5e60010619d65ab3935fc8cd62
bruce1110 <qinchong_bru@foxmail.com>
2016-06-01 14:29:30 +0800

    websocket login功能

--- imserver/Server.php
+++ imserver/Server.php
@@ -7,12 +7,106 @@
 namespace WebIm;
 use Swoole;
 
+/*
+ *服务器主要实现的功能：
+ * 1.接收直播员的比赛直播信息并广播给所有的在线观众；
+ * 2.接收所有的比赛评论信息并广播给观众；
+ * 3.需要验证直播员的身份；
+ * 4.需要验证发布评论的观众身份信息；
+ * */
 class Server extends Swoole\Protocol\WebSocket
 {
+    protected $allUsers;   //保存所有的在线观众
+    protected $loginUsers; //保存已经登录的观众
+    protected $Directors; //保存登录的直播员信息
+    protected $type = array('sendMessage','Auth','Login');
+
+    const MESSAGE_MAX_LEN = 1024; //单条消息不得超过1K
+    /*
+     * 接收到消息时
+     * type:1->直播员，2->观众
+     * 直播员进行Auth之后将client_id保存到Director；
+     * 观众进行Auth之后将client_id 保存到loginUser；
+     * Auth认证消息{"cmd":"Auth","token":"xxx","type":1/2,"id":"111222","uname":"name"}
+     * 直播员不需要login，直接进行auth认证；观众进行login之后将client_id保存到allUser；
+     * Login消息{"cmd":"Login","type":2,"logintime":"timestamp"}
+     * */
     function onMessage($client_id, $message)
     {
         // TODO: Implement onMessage() method.
-        $this->log("onMessage: ".$client_id.' = '.$message['message']);
-        $this->send($client_id, 'Server is ok!');
+        /*$this->log("onMessage: ".$client_id.' = '.$message['message']);
+        $this->send($client_id, 'Server is ok!');*/
+        $msg = json_decode($message['message'],true);
+        //判断消息是否合法，不合法消息将返回错误信息
+        if(! in_array($msg['cmd'],$this->type)||empty($msg['cmd'])||! method_exists($this,'cmd_'.$msg['cmd']))
+        {
+            $this->sendErrorMessage($client_id,'nukonwm message',101);
+            return;
+        }
+        //处理消息
+        $func = 'cmd_'.$msg['cmd'];
+        $this->$func($client_id,$msg);
+    }
+    /*----------------receivedMessage----------*/
+
+    /*----------------sendMessage--------------*/
+    /*
+     * 发送信息
+     * */
+    function cmd_sendMessage($client_id,$msg)
+    {
+
+    }
+    /**
+     * 发送错误信息
+     * message format {"code":101,"cmd":"error","msg":"xxxx"}
+    */
+    function sendErrorMessage($client_id,$msg,$code)
+    {
+        $this->sendJson($client_id,array('code'=>$code,'cmd'=>'error','msg'=>$msg));
+    }
+    /*
+     * 发送json信息
+     * */
+    function sendJson($client_id,$array)
+    {
+        $msg = json_encode($array);
+        if ($this->send($client_id, $msg) === false) {
+            $this->close($client_id);
+        }
+    }
+    /*
+     * 广播发送信息
+     * */
+    function broadCast()
+    {
+
+    }
+    /*---------------------------------END sendMessage-----------------*/
+    /*
+     * 用户身份认证
+     * */
+    function cmd_Auth($client_id,$msg)
+    {
+        return true;
+    }
+    /*
+     * 观众上线
+     * 将client_id保存到allusers
+     * 并返回登录成功信息
+     * */
+    function cmd_Login($client_id,$msg)
+    {
+        $loginsuccess=array(
+            "code"=>1,
+            "cmd"=>"login",
+            "fd"=>$client_id,
+            "msg"=>"success",
+            "logintime"=>$msg['logintime']
+        );
+        //保存会话信息
+        $this->allUsers[$client_id]=$loginsuccess;
+        //返回登录成功信息
+        $this->sendJson($client_id,$loginsuccess);
     }
 }
