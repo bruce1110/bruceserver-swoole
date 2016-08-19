c1e5a662e345864c08bd9d6428891b59beebd2c3
bruce <qinchong_bru@foxmail.com>
2016-08-19 14:18:44 +0800

    aaa

--- pool/testClient.php
+++ pool/testClient.php
@@ -1,36 +1,41 @@
 <?php
-use Pool\mySql\myPool;
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/mySql/myPool.php';
-/**
- * Created by PhpStorm.
- * User: qinchong
- * Date: 2016/8/19
- * Time: 11:02
- */
-$db = new swoole_mysql;
-$server = array(
-    'host' => '10.1.20.84',
-    'user' => 'bruce',
-    'password' => '12345',
-    'database' => 'soap',
-);
+$pool = new Pool\mySql\Pool(10);
 
-$db->connect($server, function ($db, $r) {
-    if ($r === false) {
-        var_dump($db->connect_errno, $db->connect_error);
-        die;
-    }
-    $sql = 'show tables';
-    $db->query($sql, function (swoole_mysql $db, $r) {
-        global $s;
-        if ($r === false) {
-            var_dump($db->error, $db->errno);
-        } elseif ($r === true) {
-            var_dump($db->affected_rows, $db->insert_id);
-        }
-        var_dump($r);
-        $db->close();
+$pool->create(function () use ($pool) {
+    $db = new swoole_mysql;
+    $server = array(
+        'host' => '10.1.20.84',
+        'user' => 'bruce',
+        'password' => '123456',
+        'database' => 'soap',
+    );
+    $db->on('close', function ($db) use ($pool) {
+        $pool->remove($db);
+    });
+    return $db->connect($server, function ($db, $result) use ($pool) {
+        $pool->join($db);
     });
 });
+
+for ($i = 0; $i < 10; $i++) {
+    $pool->request(function ($db) use ($pool) {
+        $r = $db->query("show tables", function (swoole_mysql $db, $r) use ($pool) {
+            global $s;
+            if ($r === false) {
+                var_dump($db->error, $db->errno);
+            } elseif ($r === true) {
+                var_dump($db->affected_rows, $db->insert_id);
+            }
+            echo "count=" . count($r) . ", time=" . (microtime(true) - $s), "\n";
+            //var_dump($r);
+            //ÊÍ·Å×ÊÔ´
+            $pool->release($db);
+        });
+
+        if ($r == false) {
+            die("xxx\n");
+        }
+
+    });
+}
