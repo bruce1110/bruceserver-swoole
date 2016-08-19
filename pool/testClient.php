7667c62994a93cb8bb57983c89ba4d1d8353694b
bruce <qinchong_bru@foxmail.com>
2016-08-19 11:47:40 +0800

    test

--- pool/testClient.php
+++ pool/testClient.php
@@ -2,18 +2,35 @@
 use Pool\mySql\myPool;
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/mySql/myPool.php';
 /**
  * Created by PhpStorm.
  * User: qinchong
  * Date: 2016/8/19
  * Time: 11:02
  */
-
-$config = array(
+$db = new swoole_mysql;
+$server = array(
     'host' => '10.2.2.105',
-    'database' => 'soap',
     'user' => 'bruce',
-    'password' => '123456'
+    'password' => '12345',
+    'database' => 'soap',
 );
-$pool = new myPool($config);
-var_dump($pool);
+
+$db->connect($server, function ($db, $r) {
+    if ($r === false) {
+        var_dump($db->connect_errno, $db->connect_error);
+        die;
+    }
+    $sql = 'show tables';
+    $db->query($sql, function (swoole_mysql $db, $r) {
+        global $s;
+        if ($r === false) {
+            var_dump($db->error, $db->errno);
+        } elseif ($r === true) {
+            var_dump($db->affected_rows, $db->insert_id);
+        }
+        var_dump($r);
+        $db->close();
+    });
+});
